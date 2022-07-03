72d832a81cc181a6b5638de8d4cc4de79b7aa34a
BasharIraqi <bashar.oroq@hotmail.com>
2022-07-03 20:05:23 +0300

    finish

---
+++ src/Components/ListLinks.jsx
@@ -0,0 +1,85 @@
+import React from "react";
+
+
+export default class ListLinks extends React.Component {
+
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            links: [], title: "", url: "", show: true,
+            editTitle: "", editUrl: "", editId: ""
+        };
+    }
+
+    handleSubmit = (e) => {
+        e.preventDefault();
+        const link = { id: Math.random() * 10, title: this.state.title, url: this.state.url };
+        this.setState({ links: [...this.state.links, link] });
+        this.setState({ title: '' });
+        this.setState({ url: '' });
+    }
+    handleTitle = (e) => {
+        this.setState({ title: e });
+    }
+    handleUrl = (e) => {
+        this.setState({ url: e });
+    }
+    handleEditTitle = (e) => {
+        this.setState({ editTitle: e });
+    }
+    handleEditUrl = (e) => {
+        this.setState({ editUrl: e });
+    }
+    handleRemove = (e) => {
+        const links = this.state.links.filter(link => link.id !== e);
+        this.setState({ links: links });
+    }
+    handleEdit = (e) => {
+        this.setState({ show: false });
+        this.setState({ editId: e });
+    }
+    handleChanges = (e) => {
+        e.preventDefault();
+        const link = this.state.links.find(link => link.id === this.state.editId);
+        link.title = this.state.editTitle;
+        link.url = this.state.editUrl;
+        this.setState({ links: this.state.links });
+        this.setState({ show: true });
+        this.setState({ editTitle: '' });
+        this.setState({ editUrl: '' });
+    }
+
+
+    render() {
+        return (
+            <>
+                <div>
+
+                    <ul>
+                        {this.state.links.map(link => (
+                            <li key={link.id}>
+                                <a href={link.url}>{link.title}</a>
+                                <button onClick={e => this.handleRemove(link.id)}>Remove</button>
+                                <button onClick={e => this.handleEdit(link.id)}>Edit</button>
+                            </li>
+                        ))}
+                        <form hidden={this.state.show} onSubmit={this.handleChanges}>
+                            <input type="text" value={this.state.editTitle} onChange={e => this.handleEditTitle(e.target.value)} />
+                            <input type="text" value={this.state.editUrl} onChange={e => this.handleEditUrl(e.target.value)} />
+                            <button type="submit">ok</button>
+                        </form>
+                    </ul>
+
+                </div>
+                <div>
+                    <form onSubmit={this.handleSubmit}>
+                        <input value={this.state.title} onChange={e => this.handleTitle(e.target.value)} type="text" placeholder="title" />
+                        <input value={this.state.url} onChange={e => this.handleUrl(e.target.value)} type="text" placeholder="url" />
+                        <button type="submit">Add</button>
+                    </form>
+                </div>
+            </>
+        );
+    }
+}
